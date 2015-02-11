359d51d83515b83254a506e4a9e5ab9aa005ad72
Jaap Groeneveld <jgroeneveld@me.com>
2015-02-11 15:53:13 +0100

    simple echo websocket

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,29 @@
-var app = {};
+var app = {
+  WEBSOCKET_URL: "ws://127.0.0.1:3000/listen",
+};
 
 (function() {
   app.init = function () {
-      console.log("initializitng javascript stuff");
+      console.log("initializing javascript app");
+      app.connect();
+  };
+
+  app.connect = function () {
+    var con = new WebSocket(app.WEBSOCKET_URL)
+    console.log("connecting");
+
+    con.onopen = function () {
+      console.log("onopen");
+      con.send("Ping"); 
+    };
+
+    con.onerror = function (error) {
+      console.log("onerror " + error);
+    };
+
+    con.onmessage = function (e) {
+      console.log("onmessage " + e.data);
+    };
   };
 })();
 
