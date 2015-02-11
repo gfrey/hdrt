d2301820466b6fa67439e51ee5ba94693509a119
Gereon Frey <gereon.frey@dynport.de>
2015-02-11 17:23:52 +0100

    improved loop in listen_handler for ws

--- server/listen_handler.go
+++ server/listen_handler.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gfrey/hdrt"
 	"golang.org/x/net/websocket"
@@ -12,20 +14,39 @@ func ListenHandler(renderDir string) http.Handler {
 		err := func() error {
 			logger.Printf("web socket connection!! renderDir: %s ", renderDir)
 
-			r := new(hdrt.Renderer)
-			err := r.LoadWorldFromReader(ws)
-			if err != nil {
-				return err
-			}
+			var msg string
+			var r *hdrt.Renderer
+			for {
+				websocket.Message.Receive(ws, &msg)
+				switch {
+				case strings.HasPrefix(msg, "ABORT"):
+					log.Printf("aborting everything")
+					if r != nil {
+						r.Abort()
+					}
+					return nil
+				case strings.HasPrefix(msg, "CFG"):
+					r = new(hdrt.Renderer)
 
-			ch, err := r.Render(renderDir)
-			if err != nil {
-				return err
-			}
+					buf := strings.NewReader(msg[3:])
+					err := r.LoadWorldFromReader(buf)
+					if err != nil {
+						WSError(ws, err)
+						continue
+					}
 
-			for imagePath := range ch {
-				logger.Printf("got image path %s", imagePath)
-				ws.Write([]byte(imagePath))
+					logger.Printf("--> start rendering")
+					ch, err := r.Render(renderDir)
+					if err != nil {
+						WSError(ws, err)
+						continue
+					}
+
+					for imagePath := range ch {
+						logger.Printf("send image path %s", imagePath)
+						websocket.Message.Send(ws, "IMG"+imagePath)
+					}
+				}
 			}
 
 			return nil
