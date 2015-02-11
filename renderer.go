3fd0b32b197dc0092370996819fcfc352344df64
Gereon Frey <gereon.frey@dynport.de>
2015-02-11 16:11:58 +0100

    added basic renderer

---
+++ renderer.go
@@ -0,0 +1,46 @@
+package hdrt
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Renderer struct {
+	wrld   *World
+	evChan chan string
+	abort  chan struct{}
+}
+
+func (r *Renderer) Render(path string) (<-chan string, error) {
+	if r.wrld == nil {
+		return nil, fmt.Errorf("world not loaded yet")
+	}
+
+	r.evChan = make(chan string)
+	r.abort = make(chan struct{})
+
+	go r.wrld.Render(r.evChan, r.abort, path)
+
+	return r.evChan, nil
+}
+
+func (r *Renderer) LoadWorldFromFile(filename string) error {
+	fh, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %s", filename, err)
+	}
+	defer fh.Close()
+
+	return r.LoadWorldFromReader(fh)
+}
+
+func (r *Renderer) LoadWorldFromReader(rd io.Reader) error {
+	r.wrld = new(World)
+	err := json.NewDecoder(rd).Decode(&r.wrld)
+	if err != nil {
+		return fmt.Errorf("failed to decode world: %s", err)
+	}
+	return nil
+}
--- world.go
+++ world.go
@@ -1,7 +1,58 @@
 package hdrt
 
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"log"
+)
+
 type World struct {
 	Camera    *Camera
 	Viewplane *Viewplane
 	Scene     *Scene
+}
+
+func (wrld *World) Render(evChan chan<- string, abortChan <-chan struct{}, path string) {
+	for i := 0; i < 10; i++ {
+		select {
+		case <-abortChan:
+			log.Printf("aborting computation")
+			return
+		default:
+			err := func() error {
+				fh, err := ioutil.TempFile(path, "img")
+				if err != nil {
+					return fmt.Errorf("failed to create file: %s", err)
+				}
+
+				img := image.NewRGBA(image.Rect(0, 0, 400, 200))
+				for x := 0; x < 400; x++ {
+					for y := 0; y < 200; y++ {
+						img.Set(x, y, color.RGBA{uint8(255 / i), uint8(255 / i), uint8(255 / i), 255})
+					}
+				}
+
+				err = png.Encode(fh, img)
+				if err != nil {
+					return fmt.Errorf("failed to create file: %s", err)
+				}
+				filename := fh.Name()
+				err = fh.Close()
+				if err != nil {
+					return fmt.Errorf("failed to close file: %s", err)
+				}
+
+				evChan <- filename
+
+				return nil
+			}()
+			if err != nil {
+				log.Printf("ERR: %s", err)
+			}
+		}
+	}
+	close(evChan)
 }
