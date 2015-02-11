aa18b69c042af19ee2cfd61c1946c849214bac84
Gereon Frey <gereon.frey@dynport.de>
2015-02-11 17:04:37 +0100

    prepared pixel wise ray casting in parallel

--- world.go
+++ world.go
@@ -7,6 +7,7 @@ import (
 	"image/png"
 	"io/ioutil"
 	"log"
+	"time"
 )
 
 type World struct {
@@ -15,30 +16,60 @@ type World struct {
 	Scene     *Scene
 }
 
+type pixel struct {
+	x, y int
+	col  *color.RGBA
+	dir  *Vector
+}
+
+const NUM_PARALLEL = 8
+
 func (wrld *World) Render(evChan chan<- string, abortChan <-chan struct{}, path string) {
-	for i := 0; i < 10; i++ {
+	pixelInChan := make(chan *pixel)
+	pixelOutChan := make(chan *pixel)
+	go func(pc chan<- *pixel) {
+		for x := 0; x < wrld.Viewplane.Width; x++ {
+			for y := 0; y < wrld.Viewplane.Height; y++ {
+				pc <- &pixel{x: x, y: y, dir: wrld.dirForPixel(x, y)}
+			}
+		}
+	}(pixelInChan)
+
+	for i := 0; i < NUM_PARALLEL; i++ {
+		go func(pinc <-chan *pixel, poutc chan<- *pixel) {
+			for pxl := range pinc {
+				wrld.renderPixel(pxl)
+				poutc <- pxl
+			}
+		}(pixelInChan, pixelOutChan)
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, wrld.Viewplane.Width, wrld.Viewplane.Height))
+	ticker := time.Tick(2 * time.Second)
+RENDER_LOOP:
+	for {
 		select {
 		case <-abortChan:
+			close(pixelInChan)
 			log.Printf("aborting computation")
 			return
-		default:
+		case pxl, ok := <-pixelOutChan:
+			if !ok {
+				break RENDER_LOOP
+			}
+			img.Set(pxl.x, pxl.y, pxl.col)
+		case <-ticker:
 			err := func() error {
 				fh, err := ioutil.TempFile(path, "img")
 				if err != nil {
 					return fmt.Errorf("failed to create file: %s", err)
 				}
 
-				img := image.NewRGBA(image.Rect(0, 0, 400, 200))
-				for x := 0; x < 400; x++ {
-					for y := 0; y < 200; y++ {
-						img.Set(x, y, color.RGBA{uint8(255 / i), uint8(255 / i), uint8(255 / i), 255})
-					}
-				}
-
 				err = png.Encode(fh, img)
 				if err != nil {
 					return fmt.Errorf("failed to create file: %s", err)
 				}
+
 				filename := fh.Name()
 				err = fh.Close()
 				if err != nil {
@@ -46,7 +77,6 @@ func (wrld *World) Render(evChan chan<- string, abortChan <-chan struct{}, path 
 				}
 
 				evChan <- filename
-
 				return nil
 			}()
 			if err != nil {
@@ -55,4 +85,12 @@ func (wrld *World) Render(evChan chan<- string, abortChan <-chan struct{}, path 
 		}
 	}
 	close(evChan)
+}
+
+func (wrld *World) renderPixel(pxl *pixel) {
+	pxl.col = &color.RGBA{200, 0, 0, 255}
+}
+
+func (wrld *World) dirForPixel(x, y int) *Vector {
+	return nil
 }
