c296147dc88991c3d91dd6771e0d12c52c853b9d
Gereon Frey <gereon.frey@dynport.de>
2015-02-12 15:47:32 +0100

    fixed cross product

--- vector.go
+++ vector.go
@@ -78,7 +78,7 @@ func VectorCross(a, b *Vector) *Vector {
 	v := new(Vector)
 
 	v[0] = a[1]*b[2] - a[2]*b[1]
-	v[1] = a[0]*b[2] - a[2]*b[0]
+	v[1] = a[2]*b[0] - a[0]*b[2]
 	v[2] = a[0]*b[1] - a[1]*b[0]
 
 	return v
