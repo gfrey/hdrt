057657e341c9e0dc6cdde05f4a17172ef5484073
Gereon Frey <me@gereonfrey.de>
2015-03-01 19:06:53 +0100

    changed ray sphere intersection code
    This is taken from the interwebs and my computer graphics course
    script. Not sure whether that is any better, but at least it's
    only a single function.

--- obj/sphere.go
+++ obj/sphere.go
@@ -24,32 +24,28 @@ func (o *objSphere) Normal(pos *vec.Vector) *vec.Vector {
 }
 
 func (o *objSphere) Intersect(p, d *vec.Vector) *vec.Vector {
-	c := o.Position
-	vpc := vec.Sub(c, p)
+	deltaP := vec.Sub(o.Position, p)
+	dDeltaP := vec.Dot(d, deltaP)
 
-	if vec.Dot(d, vpc) < 0.0 {
-		// sphere is behind the viewplane
+	if mat.FloatLessThan(dDeltaP, 0) {
 		return nil
 	}
 
-	puv := vec.Project(vpc, d) // vpc on d
-	pc := vec.Add(p, puv)      // center of the sphere projected onto the ray
-
-	if pc.DistanceTo(c) > o.Radius {
+	lambda := vec.Sub(deltaP, vec.ScalarMultiply(d, dDeltaP)).Length()
+	if mat.FloatGreaterThan(lambda, o.Radius) {
 		return nil
 	}
 
-	pcmcl := vec.Sub(pc, c).Length()
-	dist := math.Sqrt(o.Radius*o.Radius - pcmcl*pcmcl)
+	sqrt := math.Sqrt(o.Radius*o.Radius - lambda*lambda)
+	s1 := dDeltaP - sqrt
+	s2 := dDeltaP + sqrt
 
-	var di1 float64
-	if vpc.Length() > o.Radius {
-		// ray origin is outside sphere
-		di1 = vec.Sub(pc, p).Length() - dist
-	} else {
-		// ray origin is inside sphere
-		di1 = vec.Sub(pc, p).Length() + dist
+	switch {
+	case mat.FloatLessThan(s1, 0.0), mat.FloatLessThan(s2, 0.0):
+		return nil // don't show anything if inside the sphere
+	case mat.FloatLessThan(s1, s2):
+		return vec.Add(p, vec.ScalarMultiply(d, s1))
+	default:
+		return vec.Add(p, vec.ScalarMultiply(d, s2))
 	}
-
-	return vec.Add(p, vec.ScalarMultiply(d, di1))
 }
